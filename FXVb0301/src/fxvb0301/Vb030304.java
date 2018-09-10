fb01f178bc258f4834f74556b5179fc9d26fbda3
Sven den Boer <denboer.sven@gmail.com>
2018-09-10 14:14:36 +0200

    refactored

--- FXVb0301/src/fxvb0301/Vb030304.java
+++ FXVb0301/src/fxvb0301/Vb030304.java
@@ -4,12 +4,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class Vb030304 {
-    private final int leeftijdInSeconden, 
-                      leeftijdInMinuten, 
-                      leeftijdInUren, 
-                      leeftijdInDagen, 
-                      leeftijdInWeken, 
-                      leeftijdInJaren;
+    private final int 
+            leeftijdInSeconden, 
+            leeftijdInMinuten, 
+            leeftijdInUren, 
+            leeftijdInDagen, 
+            leeftijdInWeken, 
+            leeftijdInJaren;
+    
     private final Text jaren, weken, dagen, uren, minuten, seconden;
     
     public Vb030304 (Pane p) {
@@ -20,12 +22,14 @@ public class Vb030304 {
         leeftijdInWeken = leeftijdInDagen / 7;
         leeftijdInJaren = leeftijdInWeken / 52;
         
+                
         jaren = new Text(50, 150, "Leeftijd: \n" + leeftijdInJaren + " jaar");
         weken = new Text(50, 180, leeftijdInWeken + " weken");
         dagen = new Text(50, 200,  leeftijdInDagen + " dagen");
         uren = new Text(50, 220, leeftijdInUren + " uren");
         minuten = new Text(50, 240, leeftijdInMinuten + " minuten");
         seconden = new Text(50, 260, leeftijdInSeconden + " seconden");
+        
         
         p.getChildren().addAll(jaren, weken, dagen, uren, minuten, seconden);
     }
