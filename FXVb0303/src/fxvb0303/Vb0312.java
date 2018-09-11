07dbddedbbcfbefaa17e9239a2986a40d13f41bd
Sven den Boer <denboer.sven@gmail.com>
2018-09-11 14:53:01 +0200

    added exercise 3.12

--- FXVb0303/src/fxvb0303/Vb0312.java
+++ FXVb0303/src/fxvb0303/Vb0312.java
@@ -1,7 +1,55 @@
 package fxvb0303;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 
 public class Vb0312 {
     
-
+    private final Button moveLeft, moveRight, increase, decrease;
+    private final Circle circle;
+    int x = 100;
+    
+    public Vb0312 (Pane p) {
+        circle = new Circle();
+        moveLeft = new Button("L");
+        moveLeft.relocate(150, 200);
+        moveRight = new Button("R");
+        moveRight.relocate(190, 200);
+        increase = new Button("+");
+        increase.relocate(230, 200);
+        decrease = new Button("-");
+        decrease.relocate(270, 200);
+        
+  
+        
+        
+        circle.setCenterX(x);
+        circle.setCenterY(100);
+        circle.setRadius(x);
+        circle.setFill(Color.RED);
+        
+        moveLeft.setOnAction( e-> {
+            x++;
+            circle.setCenterX(x);
+        });
+        moveRight.setOnAction( e-> {
+            x--;
+            circle.setCenterX(x);
+        });
+        increase.setOnAction( e-> {
+            x++;
+            circle.setRadius(x);
+        });        
+        
+        decrease.setOnAction( e-> {
+            x--;
+            circle.setRadius(x);
+        });
+        
+        
+        p.getChildren().addAll(circle,moveLeft, moveRight, increase, decrease);
+    }
 }
