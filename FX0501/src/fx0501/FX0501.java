769a953269982c42690fa0ce312aac9d3d63c53e
Sven den Boer <denboer.sven@gmail.com>
2018-09-28 12:16:59 +0200

    added exercise 0503

--- FX0501/src/fx0501/FX0501.java
+++ FX0501/src/fx0501/FX0501.java
@@ -20,7 +20,8 @@ public class FX0501 extends Application {
         
 //        new FXExtraPane(root);
 //        new FXEx0501(root);
-        new FXEx0502(root);
+//        new FXEx0502(root);
+        new FXEx0503(root);
     }
 
     public static void main(String[] args) {
