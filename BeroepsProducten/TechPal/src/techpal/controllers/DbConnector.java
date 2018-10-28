cbec7e6dd1ed8f9b547d42353f34ee16e25760ad
Sven den Boer <denboer.sven@gmail.com>
2018-10-28 13:56:58 +0100

    modified controllers'

--- BeroepsProducten/TechPal/src/techpal/controllers/DbConnector.java
+++ BeroepsProducten/TechPal/src/techpal/controllers/DbConnector.java
@@ -1,9 +1,6 @@
 package techpal.controllers;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DbConnector {
 
